64d21755ec97cd909396841bbceb5916e4edd2bd
LorriU <l.ugen@hotmail.co.uk>
2024-05-20 19:57:27 -0400

    completed palindomeNumber exercise

--- Java/PalindromeNumber.java
+++ Java/PalindromeNumber.java
@@ -19,11 +19,47 @@ import static java.lang.Math.log10;
 public class PalindromeNumber {
     // Write your function here
 
+    //check if the reverse number and the orginal number match
+    public static boolean isPalindrome(int n){
+
+        //if number is less than 0 returns false
+        if (n < 0){
+            return false;
+        }
+
+        int reverse = 0;
+        int number = n;
+
+        while (n != 0) {
+
+            //finding the last digit
+            int lastDigit = n % 10;
+            reverse = (reverse * 10) + lastDigit;
+            n = n/10;   
+        }
+
+        //check if reverse and number are the same 
+        return (reverse == number); // if true returns 1
+        
+    }
 
 
     public static void main(String[] args) {
         // Call your function with the number you would like to check
+        int number1 = 12321;
+        int number2 = 43567;
 
+        if (isPalindrome(number1)){
+            System.out.println(number1 + " is a Palindrome");
+        } else {
+            System.out.println(number1 + " is not a Palindrome number");
+        }
+
+        if (isPalindrome(number2)){
+            System.out.println(number2 + " is a Palindrome");
+        } else {
+            System.out.println(number2 + " is not a Palindrome number");
+        }
 
     }
 }
