1fb03c7407f3d5c8e417476d4f3683a6705dcf87
LorriU <l.ugen@hotmail.co.uk>
2024-05-20 21:53:59 -0400

    corrected intToString

--- Java/IntToString.java
+++ Java/IntToString.java
@@ -9,12 +9,35 @@
 public class IntToString {
     public static String intToString(int num) {
         // Write your code here
+
+        if (num == 0){
+            return "0";
+        }
         
-        //collect number and turn into string
-        String StringNumber = Integer.toString(num);
+        //using a stringBuilder instead of built in function
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean isNegative = num < 0;
+       
+        //convert negative numbers
+        if (isNegative){
+            num = -num;
+        }
 
+        //String representation by digit
+        while (num > 0){
 
-        return StringNumber; // Modify this line to return the string representation of the integer
+            //convert digit to a character
+            stringBuilder.insert(0, (char)('0' + num % 10));
+            //remove the last digit
+            num /= 10;
+        }
+
+        //add - back to negative number
+        if (isNegative){
+            stringBuilder.insert(0, '-');
+        }
+
+        return stringBuilder.toString(); // Modify this line to return the string representation of the integer
     }
 
     public static void main(String[] args) {
